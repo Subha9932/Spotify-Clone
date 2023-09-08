b9f88ea57cb5f37f400b579dfec12b08afd1110a
Subha9932 <122509363+Subha9932@users.noreply.github.com>
2023-09-08 19:25:18 +0530

    Add files via upload

---
+++ Spotify Clone/Res.js
@@ -0,0 +1,23 @@
+console.log("Welcome to Spotify");
+let songindex=0;
+let audioElement= new Audio('resham.mp3'); 
+let audioElement1= new Audio('sadhi.mp3');   
+let audioElement2= new Audio('phir.mp3'); 
+let audioElement3= new Audio('dhoka.mp3'); 
+let audioElement4= new Audio('jana.mp3'); 
+let audioElement5= new Audio('jaan.mp3'); 
+let audioElement6= new Audio('pyaar.mp3'); 
+let audioElement7= new Audio('dhoka1.mp3'); 
+let song=[
+    {songname:"resham.mp3", filePath :"resham.mp3",coverPath:"resham.png"},
+    {songname:"dhoka.mp3",filePath:"dhoka.mp3",coverPath:"dhoka.png"},
+    {songname:"sadhi.mp3",filePath:"sadhi.mp3",coverPath:"sadhi.png"},
+    {songname:"phir.mp3",filePath:"phir.mp3",coverPath:"phir.png"},
+    {songname:"resham1.mp3",filePath:"resham1.mp3",coverPath:"resham1.png"},
+    {songname:"jana.mp3",filePath:"jana.mp3",coverPath:"jana.png"},
+    {songname:"dhoka1.mp3",filePath:"dhoka1.mp3",coverPath:"dhoka1.png"},
+    {songname:"resham.mp3",filePath:"resham.mp3",coverPath:"resham.png"},
+    {songname:"pyaar.mp3",filePath:"pyaar.mp3",coverPath:"pyaar.png"},
+    {songname:"phir.mp3",filePath:"phir.mp3",coverPath:"phir.png"},
+    {songname:"jana.mp3",filePath:"jana.mp3",coverPath:"jana.png"},
+]
